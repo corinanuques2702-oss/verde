f78b37045df4eb40fcf02cfb15675435a9e32a4a
corinanuques2702-oss <corinanuques2702@gmail.com>
2025-09-16 20:38:33 -0500

    Corregir embed de Google Maps

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -108,7 +108,7 @@ const Contact = () => {
             </h3>
             <div className="relative overflow-hidden rounded-xl shadow-lg">
               <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3987.0123456789!2d-79.8987654321!3d-2.1234567890!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMsKwMDcnMjQuNCJTIDc5wrA1Myc1NS41Ilc!5e0!3m2!1ses!2sec!4v1234567890123!5m2!1ses!2sec"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3987.0!2d-79.8987!3d-2.1234!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x902d6d0c8b8b8b8b%3A0x1234567890abcdef!2sAbel%20Castillo%20%26%20Bolivia%2C%20Guayaquil%2C%20Ecuador!5e0!3m2!1ses!2sec!4v1640995200000!5m2!1ses!2sec"
                 width="100%"
                 height="400"
                 style={{ border: 0 }}
